d9fad152ad52ab5439280199dc79dbe9ce90a5bf
Kolten Fluckiger <wrtunder@gmail.com>
2023-03-20 08:14:49 -0600

    Updating changes

--- driver/__init__.py
+++ driver/__init__.py
@@ -1,11 +1,7 @@
 from .retry import retry, retry_until_successful
 from .wait import *
 from .directory import Directory
-<<<<<<< HEAD
 from .driverinterface import Chrome, Firefox, Safari, RemoteWebdriver, DriverInterface
-=======
-from .driverinterface import Chrome, Firefox, Safari, Remote, DriverInterface
->>>>>>> f96aad86a87928e2320b812803bf9dc601f87e7b
 from .client import DriverClient, Error
 from .types import MODIFERKEYS, DROPDOWNTYPE
 from .options import ChromeOptions, FirefoxOptions
